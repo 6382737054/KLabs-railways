4b1598352af5e06cbfea75a6601e22b26f2161f4
Prasannaa E <53602368+6382737054@users.noreply.github.com>
2024-09-24 10:31:25 +0530

    Add form and UI improvements

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Login from './pages/login';
 import CreateAccount from './pages/createaccount';
 import ForgotPassword from './pages/forgotpassword';
 import Home from './pages/Home';
-import Form1 from './pages/Form1';
 import Form2 from './pages/Form2';
 import Form3 from './pages/Form3';
 import Navbar from './pages/Navbar';
 
 function App() {
+  const location = useLocation(); // Hook to get the current route
+
+  // Define the routes where the Navbar should be hidden
+  const hideNavbarRoutes = ['/', '/create-account', '/forgot-password'];
+
   return (
-    <Router>
-      <Navbar /> {/* Add Navbar here */}
+    <>
+      {/* Conditionally render the Navbar only if the current route is not in the hideNavbarRoutes array */}
+      {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
+      
       <Routes>
         {/* Login, Create Account, and Forgot Password Routes */}
         <Route path="/" element={<Login />} />
@@ -23,12 +29,19 @@ function App() {
         <Route path="/home" element={<Home />} />
 
         {/* Form Routes */}
-        <Route path="/form1" element={<Form1 />} />
         <Route path="/form2" element={<Form2 />} />
         <Route path="/form3" element={<Form3 />} />
       </Routes>
+    </>
+  );
+}
+
+function AppWrapper() {
+  return (
+    <Router>
+      <App />
     </Router>
   );
 }
 
-export default App;
+export default AppWrapper;
